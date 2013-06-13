d40d03b0bc5a454c93712291bc3dd2433baf4757
Sean Fisk <sean@seanfisk.com>
2013-06-13 15:02:27 -0600

    Add tests for invalid tokens.

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,5 +1,6 @@
 from pytest import raises
 from rply import Token
+from rply.errors import LexingError
 
 from stencil_lang.lexer import lexer
 
@@ -23,22 +24,54 @@ def assert_lex_token_list(code, expected_token_tuples):
 
 
 class TestLexer(object):
-    def test_sto_pr(self):
-        code = '''
-STO 1 32.3
-PR 2
-STO 10 -0.1
-PR 32
-'''
-        assert_lex_token_list(code, [
-            ('STO', 'STO'),
-            ('INDEX', '1'),
-            ('REAL', '32.3'),
-            ('PR', 'PR'),
-            ('INDEX', '2'),
-            ('STO', 'STO'),
-            ('INDEX', '10'),
-            ('REAL', '-0.1'),
-            ('PR', 'PR'),
-            ('INDEX', '32'),
-        ])
+    class TestValid(object):
+        def test_sto_pr(self):
+            code = '''
+    STO 1 32.3
+    PR 2
+    STO 10 -0.1
+    PR 32
+    '''
+            assert_lex_token_list(code, [
+                ('STO', 'STO'),
+                ('INDEX', '1'),
+                ('REAL', '32.3'),
+                ('PR', 'PR'),
+                ('INDEX', '2'),
+                ('STO', 'STO'),
+                ('INDEX', '10'),
+                ('REAL', '-0.1'),
+                ('PR', 'PR'),
+                ('INDEX', '32'),
+            ])
+
+    class TestInvalid(object):
+        def test_nothing(self):
+            stream = lexer.lex('')
+            with raises(StopIteration):
+                next(stream)
+
+        def test_invalid_token(self):
+            stream = lexer.lex('ABCD')
+            with raises(LexingError):
+                next(stream)
+
+        def test_invalid_sto(self):
+            stream = lexer.lex('STO 1 hello')
+            for _ in xrange(2):
+                next(stream)
+            with raises(LexingError):
+                next(stream)
+
+        def test_invalid_next_instruction(self):
+            stream = lexer.lex('STO 1 123.3\nawesome')
+            for _ in xrange(3):
+                next(stream)
+            with raises(LexingError):
+                next(stream)
+
+        def test_invalid_continues_to_raise_lexing_errors(self):
+            stream = lexer.lex('ABCD')
+            for _ in xrange(5):
+                with raises(LexingError):
+                    next(stream)
